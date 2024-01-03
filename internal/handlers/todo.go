ddfda5df456f239b2779dc82a9823f052a33d87b
Waldeedle <92062428+Waldeedle@users.noreply.github.com>
2024-01-02 20:19:33 -0500

    creating the skeleton for the CRUD operations

--- internal/handlers/todo.go
+++ internal/handlers/todo.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/waldeedle/todo/internal/templates"
@@ -16,8 +18,11 @@ func NewTodoHandler(e *echo.Echo, todoService *todo.TodoService) {
 		todoService: todoService,
 	}
 
-	e.GET("/todo", todoHandler.GetTodoHandler)
-	e.POST("/create", todoHandler.PostTodoHandler)
+	e.POST("/create", todoHandler.CreateHandler)
+	e.GET("/todo", todoHandler.GetAllHandler)
+	e.GET("/todo/:id", todoHandler.GetHandler)
+	e.POST("/todo/:id", todoHandler.UpdateHandler)
+	e.DELETE("/todo/:id", todoHandler.DeleteHandler)
 }
 
 func HTML(c echo.Context, comp templ.Component) error {
@@ -25,8 +30,39 @@ func HTML(c echo.Context, comp templ.Component) error {
 	return comp.Render(c.Request().Context(), c.Response().Writer)
 }
 
-func (t *TodoHandler) GetTodoHandler(c echo.Context) error {
-	_, err := t.todoService.GetAllTodos()
+type CreateTodoRequest struct {
+	Title string `json:"title"`
+}
+
+type GetTodoRequest struct {
+	ID int `json:"id"`
+}
+
+type UpdateTodoRequest struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+type DeleteTodoRequest struct {
+	ID int `json:"id"`
+}
+
+func (t *TodoHandler) CreateHandler(c echo.Context) error {
+	params := CreateTodoRequest{}
+	if err := c.Bind(params); err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+	err := t.todoService.Create(params.Title)
+	if err != nil {
+		return err
+	}
+
+	return HTML(c, templates.Success())
+}
+
+func (t *TodoHandler) GetAllHandler(c echo.Context) error {
+	_, err := t.todoService.GetAll()
 	if err != nil {
 		return err
 	}
@@ -34,12 +70,41 @@ func (t *TodoHandler) GetTodoHandler(c echo.Context) error {
 	return HTML(c, templates.Page())
 }
 
-func (t *TodoHandler) PostTodoHandler(c echo.Context) error {
-	title := c.FormValue("title")
-	err := t.todoService.Create(title)
+func (t *TodoHandler) GetHandler(c echo.Context) error {
+	params := GetTodoRequest{}
+	if err := c.Bind(params); err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+	_, err := t.todoService.Get(params.ID)
 	if err != nil {
 		return err
 	}
 
-	return HTML(c, templates.Success())
+	return HTML(c, templates.Page())
+}
+
+func (t *TodoHandler) UpdateHandler(c echo.Context) error {
+	params := UpdateTodoRequest{}
+	if err := c.Bind(params); err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+	err := t.todoService.Update(params.ID, params.Title, params.Completed)
+	if err != nil {
+		return err
+	}
+
+	return HTML(c, templates.Page())
+}
+
+func (t *TodoHandler) DeleteHandler(c echo.Context) error {
+	params := DeleteTodoRequest{}
+	if err := c.Bind(params); err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+	err := t.todoService.Delete(params.ID)
+	if err != nil {
+		return err
+	}
+
+	return HTML(c, templates.Page())
 }
--- internal/todo/service.go
+++ internal/todo/service.go
@@ -18,7 +18,16 @@ func NewTodoService(db *sql.DB) *TodoService {
 	}
 }
 
-func (t *TodoService) GetAllTodos() ([]Todo, error) {
+func (t *TodoService) Create(title string) error {
+	_, err := t.db.Exec("INSERT INTO todos (title, is_completed) VALUES ($1, $2)", title, false)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *TodoService) GetAll() ([]Todo, error) {
 	rows, err := t.db.Query("SELECT id, title, is_completed FROM todos")
 	if err != nil {
 		return nil, err
@@ -38,8 +47,27 @@ func (t *TodoService) GetAllTodos() ([]Todo, error) {
 	return todos, nil
 }
 
-func (t *TodoService) Create(title string) error {
-	_, err := t.db.Exec("INSERT INTO todos (title, is_completed) VALUES ($1, $2)", title, false)
+func (t *TodoService) Get(id int) (Todo, error) {
+	todo := Todo{}
+	err := t.db.QueryRow("SELECT id, title, is_completed FROM todos WHERE id = $1", id).Scan(&todo.ID, &todo.Title, &todo.Completed)
+	if err != nil {
+		return todo, err
+	}
+
+	return todo, nil
+}
+
+func (t *TodoService) Update(id int, title string, completed bool) error {
+	_, err := t.db.Exec("UPDATE todos SET title = $1, is_completed = $2 WHERE id = $3", title, completed, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *TodoService) Delete(id int) error {
+	_, err := t.db.Exec("DELETE FROM todos WHERE id = $1", id)
 	if err != nil {
 		return err
 	}
